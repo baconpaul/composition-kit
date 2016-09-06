887225c3fcfbdacaf98c515ded55683754456f34
Paul Walker <paul@pwjw.com>
2016-09-06 19:30:54 -0400

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to composition-kit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/composition_kit/core_test.clj
@@ -0,0 +1,7 @@
+(ns composition-kit.core-test
+  (:require [clojure.test :refer :all]
+            [composition-kit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
