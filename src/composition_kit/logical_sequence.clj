60de60c1a729c2349ad40abb817157710a838600
Paul Walker <paul@pwjw.com>
2016-09-12 14:57:50 -0400

    The start of a logical sequence API which I'm sure will be completely different when I write music with it; but start somewhere

---
+++ src/composition_kit/logical_sequence.clj
@@ -0,0 +1,51 @@
+(ns composition-kit.logical-sequence)
+
+;; so we start with an item which can be sequenced, defined by a pair of multimethods
+(defmulti music-item?    (fn [it] (:itemtype it)))
+(defmethod music-item? :default [it] false)
+
+(defmulti item-payload  (fn [it] (:itemtype it)))
+(defmethod item-payload :default [it] (if (music-item? it) ( :payload it ) nil ))
+
+(defmulti item-time   (fn [it] (:itemtype it)))
+(defmethod item-time :default [it] (if (music-item? it) ( :time it ) nil ))
+
+;; We define two types of items; one which has a duration (like a note) and one which is simply at a point
+;; in time (like, say, a dynamics)
+
+(defn notes-with-duration [ notes dur time ]
+  {:itemtype :notes-with-duration
+   :payload  { :notes notes :dur dur }
+   :time     time
+   })
+(defmethod music-item? :notes-with-duration [it] true)
+
+(defn music-event [ event time ]
+  {:itemtype :music-event
+   :payload  event
+   :time     time
+   })
+(defmethod music-item? :music-event [it] true)
+
+;; Next we need an instrument abstraction. An instrument is a very simple thing on which we can play an
+;; event instantly. That's all of the contract. We don't actually define any instruments here, just the
+;; methods
+(defmulti instrument? (fn [it] (:instrument-type it)))
+(defmulti play-on (fn [instrument musicitem] [ (:instrument-type instrument) (:itemtype musicitem) ] ))
+
+;; Now we define an instrument-bound item which contains a subordinate item
+(defn instument-bound-item [ instrument item ]
+  {:itemtype :instrument-bound
+   :payload (fn [t] (play-on instrument item))
+   :time    (:time item)
+   })
+(defmethod music-item? :instrument-bound [it] true)
+
+
+;; so a logical sequence is simply an object respodning to first and rest where the guarantee is that
+;; (item-time first) < (item-time (first rest)) always. There are lots of ways to make these. Here are a few,
+;; but with lazy-seq you can of course do more
+
+(defn concrete-logical-sequence [items]
+  (sort-by item-time items))
+
---
+++ test/composition_kit/test_logical_sequence.clj
@@ -0,0 +1,28 @@
+(ns composition-kit.test-logical-sequence
+  (use clojure.test)
+  (require [composition-kit.logical-sequence :as ls]))
+
+(deftest items-basics
+  (let [evt (ls/music-event "an event" 123)
+        nwd (ls/notes-with-duration [ :c4 ] 1.2 4.2)]
+    (is (ls/music-item? evt))
+    (is (ls/music-item? nwd))
+
+    (is (= (ls/item-time evt) 123))
+    (is (= (ls/payload evt) "an event"))
+
+    (is (= (ls/item-time nwd) 4.2))
+    (is (= (ls/payload nwd) { :notes [ :c4 ] :dur 1.2 }))
+    )
+  )
+
+(deftest sequences
+  (let [cs1 (ls/concrete-logical-sequence
+             (map ls/notes-with-duration [ :c4 :d4 :e4 :f4 :g4 ] [ 1 1 1/2 1/2 1 ] [ 0 4 3 2 5 ]))
+        ]
+    (is (every? identity (map (fn [a b] (<  (ls/item-time a) (ls/item-time b))) cs1 (rest cs1))) "Monotonicity in time")
+    (is (= (map #(:notes (ls/item-payload %)) cs1) [ :c4 :f4 :e4 :d4 :g4 ]))
+    )
+  )
+
+
