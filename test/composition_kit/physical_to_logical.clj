caa505ba37744a0245ecbd7c6e6e76a128b64ba9
Paul Walker <paul@pwjw.com>
2016-09-15 09:35:38 -0400

    Basics of l2p working

--- test/composition_kit/physical_to_logical.clj
+++ test/composition_kit/physical_to_logical.clj
@@ -1,13 +1,29 @@
 (ns composition-kit.physical-to-logical
   (:require [composition-kit.midi-util :as midi])
+  (:require [composition-kit.tempo :as tempo])
   (:require [composition-kit.physical-sequence :as ps])
+  (:require [composition-kit.logical-sequence :as ls])
   )
 
 (defn schedule-logical-on-physical
   [in-seq pattern instrument clock]
-  ;; This is basically a massive reduce statement
+  ;; This is basically a massive reduce statement on a big switch based on item type which then
+  ;; does the magic
   (reduce (fn [pseq item]
-            pseq
+            (case (:itemtype item)
+              :composition-kit.logical-sequence/notes-with-duration
+              (let [payload  (ls/item-payload item)
+                    notecont (:notes payload)
+                    notes    (if (coll? notecont) notecont [ notecont ] )
+                    hold-for (:hold-for payload)
+                    start-time (* 1000 (tempo/beats-to-time clock (ls/item-beat item)))
+                    end-time   (* 1000 (tempo/beats-to-time clock (+ hold-for (ls/item-beat item))))
+                    ons      (reduce (fn [s e] (ps/add-to-sequence s (fn [t] (println "note on " e " " t )) start-time)) pseq notes)
+                    offs     (reduce (fn [s e] (ps/add-to-sequence s (fn [t] (println "note off " e " " t)) end-time)) ons notes)
+                    ]
+                offs
+                )
+              )
             )
           in-seq
           pattern)
--- test/composition_kit/test_phys_to_log.clj
+++ test/composition_kit/test_phys_to_log.clj
@@ -14,8 +14,20 @@
         pseq   (ptol/schedule-logical-on-physical (ps/new-sequence) phrase inst clock)
         ]
     (is (= (count (:seq pseq)) 6)) ;; Note on and note off events
+    (ps/play pseq)
+    ;; Check the times match the clock beat times
+    
+    ;; Actually play the damn thing attached to a reciever and see if we get back the three notes at roughly appropriate times
     )
   )
 
 
+
+(let [phrase (ls/sequence-from-pitches-and-durations [ :c4 :d4 :e4 ] [ 1 1/2 1/2 ] )
+      inst   (midi/midi-instrument 0)
+      clock  (tempo/constant-tempo 2 4 140)
+      pseq   (ptol/schedule-logical-on-physical (ps/new-sequence) phrase inst clock)
+      ]
+
+  )
 
