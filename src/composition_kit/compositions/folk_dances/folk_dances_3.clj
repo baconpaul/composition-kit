a05713f45210889c15ef1578f83e36adf61dc7cb
Paul Walker <paul@pwjw.com>
2016-10-24 09:33:25 -0400

    FD3 piano in reasonable initial shape

--- src/composition_kit/compositions/folk_dances/folk_dances_3.clj
+++ src/composition_kit/compositions/folk_dances/folk_dances_3.clj
@@ -1,6 +1,7 @@
 (ns composition-kit.compositions.folk-dances.folk-dances-3
   (:require [composition-kit.music-lib.midi-util :as midi])
   (:require [composition-kit.music-lib.tempo :as tempo])
+  (:require [composition-kit.music-lib.tonal-theory :as th])
   (:require [composition-kit.music-lib.logical-sequence :as ls])
   (:require [composition-kit.music-lib.logical-item :as i])
 
@@ -52,7 +53,7 @@
   <c ees bes'>4   <c ees bes'>4   <c ees bes'>4   <c ees c'>2. <c ees g>2
   
   <c ees bes'>4   <c ees bes'>4   <c ees bes'>4   <c ees c'>1
-  <c ees bes'>4   <c ees bes'>4   <c ees bes'>4   <c ees aes>1 r4")
+  <c ees bes'>4   <c ees bes'>4   <c ees bes'>4   <c ees aes>1")
 
 (def orig-piano-lh
   "
@@ -74,25 +75,82 @@
    aes,4 aes aes aes1 aes4 aes aes aes2. aes2
    aes4 aes aes aes1 aes4 aes aes aes2. aes2
    des4 des des des1 des4 des des des2. des2
-   aes4 aes aes aes1 aes4 aes aes aes1 r4" )
+   aes4 aes aes aes1 aes4 aes aes aes1" )
+
 
 
 (def orig-piano
-  (-*>
-   (<*>
-    (-*> (lily orig-piano-rh :relative :c3)
-         (ls/hold-for-pct 0.98)
-         )
-    (let [ls (lily orig-piano-lh :relative :c3)]
-      (-*>
-       (<*> ls
-            (-*> ls (ls/transpose -12)))
-       (ls/hold-for-pct 0.98)
-       ))
-    (apply pedal-held-and-cleared-at (mapcat (fn [i] [(* 15 i) (+ (* 15 i) 3) (+ (* 15 i) 7) (+ (* 15 i) 10)]) (range 15)))
-    )
-   (ls/transform :dynamics (fn [i] (constantly 64)))
-   ))
+  (let [in-measure-dyn      (partition 2 [0 1  4 1.2  5 0.8  11 1.15  12 0.6]) ;; this pattern is bad for measures 9 - 11
+        mid-measure-dyn     (partition 2 [0 1 3 0.9 6 1.05 6.9 1.02 9 0.97 15 1.15])
+        final-mid-measure   (partition 2 [0 0.95 10 1.2 12 0.8])
+
+        by-measure-dyn      (concat (repeat 8 in-measure-dyn)
+                                    (repeat 2 mid-measure-dyn)
+                                    (repeat 1 final-mid-measure)
+                                    (repeat 4 in-measure-dyn))
+        
+        measure-overall-dyn [45 47 52 47 63 64 67 64 72 76 79 45 47 52 46]
+        xpand-dyn (flatten
+                   (map (fn [id md inm]
+                          (let [ofs (* id 15)]
+                            (map (fn [i] [ (+ ofs (first i)) (* (second i) md)]) inm)))
+                        (range (count measure-overall-dyn)) measure-overall-dyn by-measure-dyn))
+
+        rh-orig
+        (-*> (lily orig-piano-rh :relative :c3)
+             (ls/hold-for-pct 0.99)
+             ((fn [s] (apply ls/line-segment-dynamics (concat [s] xpand-dyn)))))
+
+
+        nff
+        (fn [op]
+          (fn [i p]
+            (if (seq? (:notes p))
+              ;; find the highest
+              (let [n (:notes p)
+                    mn (map th/name-to-midinote n)
+                    top (apply max mn)
+                    r   (op #(= (th/name-to-midinote %) top) n)
+                    ]
+                (assoc p :notes r))
+              p)))
+        
+        rh-top-note
+        (-*> rh-orig
+             (ls/transform-note-payload (nff filter)))
+
+        rh-other-notes 
+        (-*> rh-orig
+             (ls/transform-note-payload (nff remove)))
+
+        rh
+        (<*>
+         (-*> rh-top-note (ls/amplify 1.05))
+         (-*> rh-other-notes (ls/amplify 0.95)))
+        
+        lh
+        (let [ls (lily orig-piano-lh :relative :c3)]
+          (-*>
+           (<*> ls
+                (-*> ls (ls/transpose -12)))
+           (ls/hold-for-pct 0.98)
+           ((fn [s] (apply ls/line-segment-dynamics (concat [s] xpand-dyn))))
+           (ls/amplify 0.87)
+           )
+          )
+
+        ]
+    
+    (-*>
+     (<*>
+      rh
+      lh
+      (apply pedal-held-and-cleared-at (mapcat (fn [i] [(* 15 i) (+ (* 15 i) 3) (+ (* 15 i) 7) (+ (* 15 i) 10)]) (range 15)))
+      )
+
+     )
+    ))
+
 
 (def final-song
   (->
@@ -106,8 +164,8 @@
   (when playit
     (midi-play
      final-song
-     :beat-zero 70
-     ;;:beat-end 120
+     ;;:beat-zero (* 8 15)
+     ;;:beat-end (* 12 15)
      )))
 
 ;;(def s (composition-kit.events.physical-sequence/stop player))
